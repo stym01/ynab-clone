30fcca9d4fefc458e017f906297cf67d330b898a
Satyam Kesharwani <satyamkesharwani134@gmail.com>
2026-05-30 06:32:25 +0530

    Remove Gmail API scope from NextAuth

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -11,7 +11,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
-          scope: 'openid email profile https://www.googleapis.com/auth/gmail.readonly',
+          scope: 'openid email profile',
           access_type: 'offline',
           prompt: 'consent',
         },
